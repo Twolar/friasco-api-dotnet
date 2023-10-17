b02bdf1977bf71f8d26d65b5289bbe8bdecbcc3b
“Taylor <t4ybennett@gmail.com>
2023-10-17 19:51:47 +0100

    init

---
+++ friasco-api-test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace friasco_api_test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
