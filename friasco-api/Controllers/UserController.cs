604e5c29f533924ee9d8c8291ea662ea11efb351
“Taylor <t4ybennett@gmail.com>
2023-10-18 11:59:23 +0100

    Endpoint controller scaffold

---
+++ friasco-api/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace friasco_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    // Create
+    // Read
+    // Update
+    // Delete
+}
