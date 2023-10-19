f1bc0afb25f1de0888001a6fad3498caffb0bbef
“Taylor <t4ybennett@gmail.com>
2023-10-19 18:53:01 +0100

    Add SQLite database

---
+++ friasco-api-integration-tests/IntegrationTestBase.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace friasco_api_integration_tests;
+
+public class IntegrationTestBase
+{
+    #region Helper Methods
+
+    public async Task AddUserSqlite(
+        SqliteConnection connection,
+        string username,
+        string firstName,
+        string lastName,
+        string email,
+        int role,
+        string passwordHash)
+    {
+        string sql = @"
+        INSERT INTO Users (Username, FirstName, LastName, Email, Role, PasswordHash) 
+        VALUES (@Username, @FirstName, @LastName, @Email, @Role, @PasswordHash);
+        ";
+
+        var user = new
+        {
+            Username = username,
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            Role = role,
+            PasswordHash = passwordHash
+        };
+
+        await connection.ExecuteAsync(sql, user);
+    }
+
+
+    #endregion
+}
--- friasco-api/Program.cs
+++ friasco-api/Program.cs
@@ -1,6 +1,16 @@
+using friasco_api.Data;
+using Microsoft.Data.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Database connection string + Add DataContext + Inject connection method into DataContext constructor
+var connectionString = builder.Configuration.GetConnectionString("FriascoDatabase");
+builder.Services.AddScoped<IDataContext, DataContext>(serviceProvider =>
+{
+    return new DataContext(() => new SqliteConnection(connectionString));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +18,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Ensure database and tables exist
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<IDataContext>();
+    await context.InitDatabase();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
