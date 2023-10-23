d21c51718daf49690eb82801ac52e13e4a242ab3
“Taylor <t4ybennett@gmail.com>
2023-10-23 11:41:11 +0100

    Add some userController tests + test scaffolding

--- friasco-api-integration-tests/IntegrationTestBase.cs
+++ friasco-api-integration-tests/IntegrationTestBase.cs
@@ -1,11 +1,34 @@
 ﻿using Dapper;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace friasco_api_integration_tests;
 
 public class IntegrationTestBase
 {
+    private WebApplicationFactory<Program> _factory;
+    
     #region Helper Methods
+
+    public HttpClient CreateHttpClient()
+    {
+        _factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    // TODO: Add services here...
+                });
+            });
+        return _factory.CreateClient();
+    }
+
+    public void CleanUpHttpClient(HttpClient client)
+    {
+        client.Dispose();
+        _factory.Dispose();
+    }
 
     public async Task AddUserSqlite(
         SqliteConnection connection,
---
+++ friasco-api-unit-tests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using friasco_api.Controllers;
+
+namespace friasco_api_unit_tests.Controllers;
+
+[TestFixture]
+public class UsersControllerTests
+{
+    private Mock<ILogger<UsersController>> _loggerMock;
+    private UsersController _controller;
+
+    [SetUp]
+    public void Setup()
+    {
+        _loggerMock = new Mock<ILogger<UsersController>>();
+        _controller = new UsersController(_loggerMock.Object);
+    }
+
+    [Test]
+    public async Task GetAll_ReturnsOkResult()
+    {
+        var result = await _controller.GetAll();
+        Assert.IsInstanceOf<OkResult>(result);
+    }
+
+    [Test]
+    public async Task GetById_ReturnsOkResult()
+    {
+        var result = await _controller.GetById(1);
+        Assert.IsInstanceOf<OkResult>(result);
+    }
+
+    [Test]
+    public async Task Create_ReturnsOkResult() {
+        var result = await _controller.Create();
+        Assert.IsInstanceOf<OkResult>(result);
+    }
+
+    public async Task Update_ReturnsOkResult() {
+        var result = await _controller.Update(1);
+        Assert.IsInstanceOf<OkResult>(result);
+    }
+
+    public async Task Delete_ReturnsOkResult() {
+        var result = await _controller.Delete(1);
+        Assert.IsInstanceOf<OkResult>(result);
+    }
+}
