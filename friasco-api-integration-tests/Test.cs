e8fb232e0f5a926e9e47b2988e43dd437ac5ef90
“Taylor <t4ybennett@gmail.com>
2023-10-26 14:36:48 +0100

    Get integration test project working and making successful calls to API

--- friasco-api-integration-tests/Test.cs
+++ friasco-api-integration-tests/Test.cs
@@ -1,4 +1,10 @@
-﻿namespace friasco_api_integration_tests;
+﻿using System.Text;
+using System.Text.Json;
+using friasco_api.Enums;
+using friasco_api.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace friasco_api_integration_tests;
 
 [TestFixture]
 public class Test
@@ -12,5 +18,30 @@ public class Test
     [Test]
     public async Task TestTemporary()
     {
+        // TODO: Seperate database for integration tests either by :inmemory: or standalone
+        var factory = new WebApplicationFactory<Program>();
+
+        var client = factory.CreateClient();
+
+        var userCreateRequestModel = new UserCreateRequestModel
+        {
+            Username = "intUser1",
+            Email = "user@example.com",
+            FirstName = "intuser1First",
+            LastName = "intuser1Last",
+            Role = UserRoleEnum.User,
+            Password = "intpassword123",
+            ConfirmPassword = "intpassword123"
+        };
+
+        var jsonContent = new StringContent(JsonSerializer.Serialize(userCreateRequestModel), Encoding.UTF8, "application/json");
+
+        await client.PostAsync("/users", jsonContent);
+
+        var response = await client.GetAsync("/users");
+
+        var users = response.Content.ReadAsStringAsync();
+
+        Console.WriteLine(users);
     }
 }
