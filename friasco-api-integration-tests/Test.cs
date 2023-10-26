d46f91037bbe1ddc172ff6a6d2761fe6c0c0b42a
“Taylor <t4ybennett@gmail.com>
2023-10-26 13:33:00 +0100

    Test file/method

---
+++ friasco-api-integration-tests/Test.cs
@@ -0,0 +1,16 @@
+﻿namespace friasco_api_integration_tests;
+
+[TestFixture]
+public class Test
+{
+    [SetUp]
+    public void SetUp()
+    {
+
+    }
+
+    [Test]
+    public async Task TestTemporary()
+    {
+    }
+}
