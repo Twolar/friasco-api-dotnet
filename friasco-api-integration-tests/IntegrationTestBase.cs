b1a94fe26d291db7cc2283adf148aee6d5322e4f
“Taylor <t4ybennett@gmail.com>
2023-10-26 18:18:33 +0100

    Integration test base class + helper for HttpClient

---
+++ friasco-api-integration-tests/IntegrationTestBase.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+using friasco_api.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace friasco_api_integration_tests;
+
+[TestFixture]
+public class IntegrationTestBase
+{
+    private WebApplicationFactory<Program> _factory;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _factory = new WebApplicationFactory<Program>();
+    }
+
+    #region Helpers
+
+    protected async Task<HttpClient> GetHttpClientAsync()
+    {
+        // TODO: Look at potentially refactoring this again? i.e. Do we have to do all this everytime we want an HTTP Cleint?
+        var client = _factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Remove Friasco_Api DbContext DI
+                var apiDbContextDescription = services.SingleOrDefault(d => d.ServiceType == typeof(IDataContext));
+                if (apiDbContextDescription != null)
+                {
+                    services.Remove(apiDbContextDescription);
+                }
+
+                // Replace with our own for testing purposes
+                services.AddScoped<IDataContext, DataContext>(serviceProvider =>
+                {
+                    var testingDbPathAndName = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "FriascoDatabaseTEST.db");
+                    return new DataContext(() => new SqliteConnection($"Data Source={testingDbPathAndName}"));
+                });
+            });
+        }).CreateClient();
+
+        // Ensure database and tables exist
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<IDataContext>();
+            await context.InitDatabase();
+        }
+
+        return client;
+    }
+
+    #endregion
+}
---
+++ friasco-api-integration-tests/UserEndpointTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Text.Json;
+using friasco_api.Enums;
+using friasco_api.Models;
+
+namespace friasco_api_integration_tests;
+
+[TestFixture]
+public class UserEndpointTests : IntegrationTestBase
+{
+    [SetUp]
+    public void SetUp()
+    {
+
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+
+    }
+
+    [Test]
+    public async Task FirstTestExample()
+    {
+        var client = await GetHttpClientAsync();
+
+        try
+        {
+            var userCreateRequestModel = new UserCreateRequestModel
+            {
+                Username = "tempUser1",
+                Email = "tempuser@example.com",
+                FirstName = "tempuser1First",
+                LastName = "tempuser1Last",
+                Role = UserRoleEnum.User,
+                Password = "temppassword123",
+                ConfirmPassword = "temppassword123"
+            };
+
+            var jsonContent = new StringContent(JsonSerializer.Serialize(userCreateRequestModel), Encoding.UTF8, "application/json");
+
+            await client.PostAsync("/users", jsonContent);
+
+            var response = await client.GetAsync("/users");
+
+            var users = response.Content.ReadAsStringAsync();
+        }
+        finally
+        {
+            // TODO: Figure out a way to clean up data created
+            // Through either helper methods running sql or transactions (transactions if possible...)
+        }
+    }
+}
