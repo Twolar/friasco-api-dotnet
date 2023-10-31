753aca9885310de83c25844adc2119942b7a560d
“Taylor <t4ybennett@gmail.com>
2023-10-31 14:28:18 +0000

    Integration testing tings

--- friasco-api-integration-tests/UserEndpointTests.cs
+++ friasco-api-integration-tests/UserEndpointTests.cs
@@ -10,6 +10,8 @@ namespace friasco_api_integration_tests;
 [TestFixture]
 public class UserEndpointTests : IntegrationTestBase
 {
+    #region Users Get Tests
+
     [Test]
     public async Task Users_GetAll_Succeeds_WithMatchingDbValues()
     {
@@ -65,6 +67,8 @@ public class UserEndpointTests : IntegrationTestBase
         }
     }
 
+    // Test: Get users when empty
+
     [Test]
     public async Task Users_Get_Succeeds_WithMatchingDbValues()
     {
@@ -106,6 +110,12 @@ public class UserEndpointTests : IntegrationTestBase
             await DbUserDeleteByEmail(newUser.Email);
         }
     }
+
+    // Test: Get user when doesn't exist
+
+    #endregion
+
+    #region User Create Tests
 
     [Test]
     public async Task Users_Create_Succeeds_WithMatchingDbValues()
@@ -149,6 +159,60 @@ public class UserEndpointTests : IntegrationTestBase
             }
         }
     }
+
+    [Test]
+    public async Task Users_Create_Succeeds_WithRandomPropertiesInRequest()
+    {
+        User? userInDb = null;
+
+        try
+        {
+            var userCreateJsonObject = new
+            {
+                Username = "User1",
+                Email = "User1@example.com",
+                FirstName = "User1First",
+                LastName = "User1Last",
+                Role = UserRoleEnum.User,
+                Password = "Password123",
+                ConfirmPassword = "Password123",
+                RandomPropertyOne = "RandomPropertyOne",
+                RandomPropertyTwo = "RandomPropertyTwo",
+                RandomPropertyThree = "RandomPropertyThree"
+            };
+
+            var jsonContent = new StringContent(JsonSerializer.Serialize(userCreateJsonObject), Encoding.UTF8, "application/json");
+
+            var response = await Client.PostAsync("/users", jsonContent);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.IsSuccessStatusCode, Is.EqualTo(true));
+
+            userInDb = await DbUserGetByEmail(userCreateJsonObject.Email);
+
+            Assert.That(userInDb, Is.Not.EqualTo(null));
+            Assert.That(userInDb.Username, Is.EqualTo(userCreateJsonObject.Username));
+            Assert.That(userInDb.Email, Is.EqualTo(userCreateJsonObject.Email));
+            Assert.That(userInDb.FirstName, Is.EqualTo(userCreateJsonObject.FirstName));
+            Assert.That(userInDb.LastName, Is.EqualTo(userCreateJsonObject.LastName));
+            Assert.That(userInDb.Role, Is.EqualTo(userCreateJsonObject.Role));
+            Assert.That(userInDb.PasswordHash, Is.Not.EqualTo(null));
+        }
+        finally
+        {
+            if (userInDb != null)
+            {
+                await DbUserDeleteById(userInDb.Id);
+            }
+        }
+    }
+
+    // Test: Create user with missing fields
+    // Test: Create duplicate user or user with same email
+    // Test: Create user with non-matching passwords
+
+    #endregion
+
+    #region Users Update Tests
 
     [Test]
     public async Task Users_Update_Succeeds_WithMatchingDbValues()
@@ -209,6 +273,102 @@ public class UserEndpointTests : IntegrationTestBase
     }
 
     [Test]
+    public async Task Users_Update_Succeeds_WithRandomPropertiesInRequest()
+    {
+        User? userInDb = null;
+        var newUser = new User
+        {
+            Username = "User1",
+            Email = "uusUser1@example.com",
+            FirstName = "User1First",
+            LastName = "User1Last",
+            Role = UserRoleEnum.User,
+            PasswordHash = "PasswordHash123",
+        };
+
+        try
+        {
+            await DbUserCreate(newUser);
+
+            userInDb = await DbUserGetByEmail(newUser.Email);
+            Assert.That(userInDb, Is.Not.EqualTo(null));
+            var userInDbPasswordHashBeforeUpdate = userInDb.PasswordHash;
+
+            var updateUserObject = new
+            {
+                Username = "updatedUser1",
+                Email = "uusUpdatedUser1@example.com",
+                FirstName = "updatedUser1First",
+                LastName = "updatedUser1Last",
+                Role = UserRoleEnum.Admin,
+                Password = "updatedPasswordHash123",
+                ConfirmPassword = "updatedPasswordHash123",
+                RandomPropertyOne = "RandomPropertyOne",
+                RandomPropertyTwo = "RandomPropertyTwo",
+                RandomPropertyThree = "RandomPropertyThree"
+            };
+            JsonContent updateUserContent = JsonContent.Create(updateUserObject);
+
+            var response = await Client.PutAsync($"/users/{userInDb.Id}", updateUserContent);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.IsSuccessStatusCode, Is.EqualTo(true));
+
+            userInDb = await DbUserGetById(userInDb.Id);
+
+            Assert.That(userInDb, Is.Not.EqualTo(null));
+            Assert.That(userInDb.Username, Is.EqualTo(updateUserObject.Username));
+            Assert.That(userInDb.Email, Is.EqualTo(updateUserObject.Email));
+            Assert.That(userInDb.FirstName, Is.EqualTo(updateUserObject.FirstName));
+            Assert.That(userInDb.LastName, Is.EqualTo(updateUserObject.LastName));
+            Assert.That(userInDb.Role, Is.EqualTo(updateUserObject.Role));
+            Assert.That(userInDb.PasswordHash, Is.Not.EqualTo(userInDbPasswordHashBeforeUpdate));
+        }
+        finally
+        {
+            if (userInDb != null)
+            {
+                await DbUserDeleteById(userInDb.Id);
+
+            }
+        }
+    }
+
+    [Test]
+    public async Task Users_Update_Fails_WhenUserIdDoesNotExist()
+    {
+        try
+        {
+            var updateUserObject = new
+            {
+                Username = "updatedUser1",
+                Email = "uusUpdatedUser1@example.com",
+                FirstName = "updatedUser1First",
+                LastName = "updatedUser1Last",
+                Role = UserRoleEnum.Admin,
+                Password = "updatedPasswordHash123",
+                ConfirmPassword = "updatedPasswordHash123",
+            };
+            JsonContent updateUserContent = JsonContent.Create(updateUserObject);
+
+            var response = await Client.PutAsync($"/users/{1232131}", updateUserContent);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.InternalServerError));
+            Assert.That(response.IsSuccessStatusCode, Is.EqualTo(false));
+        }
+        finally
+        {
+        }
+    }
+
+    // Test: Update user with missing fields
+    // Test: Update user with an email that exists for another user
+    // Test: Update user without providing passwords
+    // Test: Update user with non-matching passwords
+
+    #endregion
+
+    #region Users Delete Tests
+
+    [Test]
     public async Task Users_Delete_Succeeds_WithMatchingDbValues()
     {
         User? userInDb = null;
@@ -244,4 +404,25 @@ public class UserEndpointTests : IntegrationTestBase
             }
         }
     }
+
+    [Test]
+    public async Task Users_Delete_Succeeds_WhenUserIdDoesNotExist()
+    {
+        try
+        {
+            var id = 238942893;
+
+            var response = await Client.DeleteAsync($"/users/{id}");
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.IsSuccessStatusCode, Is.EqualTo(true));
+
+            // We want to return 200 OK because user should be deleted whether it existed or not,
+            // Also don't want to tell an api client whether a user exists by that ID...
+        }
+        finally
+        {
+        }
+    }
+
+    #endregion
 }
