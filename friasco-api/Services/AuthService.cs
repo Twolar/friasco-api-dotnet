a7ed71bfb0e68a5acc9da06f884c30d0096b817e
“Taylor <t4ybennett@gmail.com>
2023-11-01 18:03:10 +0000

    Add functionality to verify user email exists and check the password hash vs supplied

--- friasco-api/Services/AuthService.cs
+++ friasco-api/Services/AuthService.cs
@@ -1,4 +1,7 @@
-﻿using friasco_api.Models;
+﻿using friasco_api.Data.Entities;
+using friasco_api.Data.Repositories;
+using friasco_api.Helpers;
+using friasco_api.Models;
 
 namespace friasco_api.Services;
 
@@ -10,26 +13,44 @@ public interface IAuthService
 public class AuthService : IAuthService
 {
     private readonly ILogger<IAuthService> _logger;
+    private readonly IUserRepository _userRepository;
 
-    public AuthService(ILogger<IAuthService> logger)
+    public AuthService(ILogger<IAuthService> logger, IUserRepository userRepository)
     {
         _logger = logger;
+        _userRepository = userRepository;
     }
 
     public async Task<string> Login(AuthLoginRequestModel model)
     {
         _logger.Log(LogLevel.Debug, "AuthService::Login");
 
-        // Verify Email & Password
+        var invalidCredentialsString = "Invalid login credentials supplied.";
 
-        var token = await GenerateToken();
+        var user = await _userRepository.GetByEmail(model.Email!);
 
-        return token;
+        if (user == null)
+        {
+            // TODO: test this outcome
+            throw new AppException(invalidCredentialsString);
+        }
+
+        // TODO: test this outcome
+        bool userVerified = BCrypt.Net.BCrypt.Verify(model.Password, user.PasswordHash);
+        if (!userVerified)
+        {
+            // TODO: test this outcome
+            throw new AppException(invalidCredentialsString);
+        }
+
+        var userToken = await GenerateToken(user);
+
+        return userToken;
     }
 
-    private async Task<string> GenerateToken()
+    private async Task<string> GenerateToken(User user)
     {
-        var token = "RandomToken";
+        var token = $"RandomTokenFor";
 
         return token;
     }
