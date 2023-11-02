3865d1f1a75fab88fb8adbe5ba271c5201e1488c
“Taylor <t4ybennett@gmail.com>
2023-11-02 17:54:27 +0000

    Add bcrypt wrapper for testing

---
+++ friasco-api/Helpers/BCryptWrapper.cs
@@ -0,0 +1,20 @@
+﻿namespace friasco_api.Helpers;
+
+public interface IBCryptWrapper
+{
+    string HashPassword(string password);
+    bool Verify(string password, string hashedPassword);
+}
+
+public class BCryptWrapper : IBCryptWrapper
+{
+    public string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool Verify(string password, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
