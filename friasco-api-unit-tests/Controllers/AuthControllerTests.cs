0ae461ad4b8c437c26372e57bfd112cf0b66f626
“Taylor <t4ybennett@gmail.com>
2023-11-02 17:55:39 +0000

    Add basic auth controller tests

--- friasco-api-unit-tests/Controllers/AuthControllerTests.cs
+++ friasco-api-unit-tests/Controllers/AuthControllerTests.cs
@@ -1,7 +1,42 @@
-﻿namespace friasco_api_unit_tests.Controllers;
+﻿using friasco_api.Controllers;
+using friasco_api.Models;
+using friasco_api.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
 
+namespace friasco_api_unit_tests.Controllers;
+
+[TestFixture]
 public class AuthControllerTests
 {
-    // Test: Login
-    // Test: Register
+    private Mock<ILogger<AuthController>> _loggerMock;
+    private Mock<IAuthService> _authServiceMock;
+    private AuthController _controller;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _loggerMock = new Mock<ILogger<AuthController>>();
+        _authServiceMock = new Mock<IAuthService>();
+        _controller = new AuthController(_loggerMock.Object, _authServiceMock.Object);
+    }
+
+    [Test]
+    public async Task Login_ReturnsOkResult()
+    {
+        var model = new AuthLoginRequestModel();
+        _authServiceMock.Setup(x => x.Login(model)).ReturnsAsync("JwtString");
+        var result = await _controller.Login(model);
+        Assert.IsInstanceOf<OkObjectResult>(result);
+    }
+
+    [Test]
+    public async Task Register_ReturnsOkResult()
+    {
+        var model = new UserCreateRequestModel();
+        _authServiceMock.Setup(x => x.Register(model)).ReturnsAsync("JwtString");
+        var result = await _controller.Register(model);
+        Assert.IsInstanceOf<OkObjectResult>(result);
+    }
 }
