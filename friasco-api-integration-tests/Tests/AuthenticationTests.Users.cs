4ddff05070be984ad6885f56f21db8e560672e96
“Taylor <t4ybennett@gmail.com>
2023-11-02 08:59:35 +0000

    Init test files for auth

---
+++ friasco-api-integration-tests/Tests/AuthenticationTests.Users.cs
@@ -0,0 +1,18 @@
+﻿namespace friasco_api_integration_tests.Tests;
+
+public partial class AuthenticationTests : IntegrationTestBase
+{
+    // TODO: Test all user route authentication
+    // GetAll
+    // GetById
+    // Create
+    // Update
+    // Delete
+
+    // Leaving here for now decide where to put them, need to test:
+    // Login
+    // Register (CreateUser)
+    // Tiered based role auth, i.e
+    //      If user is superadmin then should have access to everything below this
+    //      If user is admin should have admin and below, but not superadmin
+}
