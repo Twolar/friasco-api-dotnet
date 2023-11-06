9f150ccc72a8056144db70a75e3b250e21a11ba1
“Taylor <t4ybennett@gmail.com>
2023-11-06 12:26:38 +0000

    Add tests for login/register + refactoring tests

--- friasco-api-integration-tests/Tests/AuthEndpointTests.cs
+++ friasco-api-integration-tests/Tests/AuthEndpointTests.cs
@@ -1,264 +1,79 @@
 ﻿using System.Net;
-using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using friasco_api.Data.Entities;
 using friasco_api.Enums;
+using friasco_api_integration_tests.Helpers;
 
 namespace friasco_api_integration_tests.Tests;
 
-public partial class AuthEndpointTests : IntegrationTestBase
+public class AuthEndpointTests : IntegrationTestBase
 {
-    private static List<string> _apiClientNames = new List<string> {
-        nameof(ApiClientWithNoAuth),
-        nameof(ApiClientWithRoleUser),
-        nameof(ApiClientWithRoleAdmin),
-        nameof(ApiClientWithRoleSuperAdmin)
-    };
-
     [Test]
-    [TestCaseSource(nameof(_apiClientNames))]
-    public async Task Users_GetAll_Auth(string apiClientName)
+    public async Task Auth_Login_ReturnsOkResult_WithToken()
     {
-        var url = "/users";
-
-        HttpResponseMessage? response;
-
-        switch (apiClientName)
+        var apiUserLoginObject = new
         {
-            case nameof(ApiClientWithNoAuth):
-                response = await ApiClientWithNoAuth.GetAsync(url);
-                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
-                break;
-            case nameof(ApiClientWithRoleUser):
-                response = await ApiClientWithRoleUser.GetAsync(url);
-                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
-                break;
-            case nameof(ApiClientWithRoleAdmin):
-                response = await ApiClientWithRoleAdmin.GetAsync(url);
-                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                break;
-            case nameof(ApiClientWithRoleSuperAdmin):
-                response = await ApiClientWithRoleSuperAdmin.GetAsync(url);
-                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                break;
-            default:
-                throw new ArgumentException($"Invalid api client name: {apiClientName}");
-        }
+            Email = "UserRole@example.com",
+            Password = "Password123",
+        };
+        var jsonContent = new StringContent(JsonSerializer.Serialize(apiUserLoginObject), Encoding.UTF8, "application/json");
+
+        var response = await ApiClientWithRoleSuperAdmin.PostAsync("/login", jsonContent);
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.That(response.IsSuccessStatusCode, Is.EqualTo(true));
+
+        var contentJsonString = await response.Content.ReadAsStringAsync();
+        var loginResponse = JsonSerializer.Deserialize<LoginResponse>(contentJsonString, DefaultTestingJsonSerializerOptions);
+        Assert.That(loginResponse.Token, Is.Not.EqualTo(string.Empty));
+        Assert.That(loginResponse.Token, Is.Not.EqualTo(null));
     }
 
     [Test]
-    [TestCaseSource(nameof(_apiClientNames))]
-    public async Task Users_Get_Auth(string apiClientName)
-    {
-        var url = $"/users/{85732}";
-
-        HttpResponseMessage? response;
-
-        switch (apiClientName)
-        {
-            case nameof(ApiClientWithNoAuth):
-                response = await ApiClientWithNoAuth.GetAsync(url);
-                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
-                break;
-            case nameof(ApiClientWithRoleUser):
-                response = await ApiClientWithRoleUser.GetAsync(url);
-                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
-                break;
-            case nameof(ApiClientWithRoleAdmin):
-                response = await ApiClientWithRoleAdmin.GetAsync(url);
-                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
-                break;
-            case nameof(ApiClientWithRoleSuperAdmin):
-                response = await ApiClientWithRoleSuperAdmin.GetAsync(url);
-                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
-                break;
-            default:
-                throw new ArgumentException($"Invalid api client name: {apiClientName}");
-        }
-    }
-
-    [Test]
-    [TestCaseSource(nameof(_apiClientNames))]
-    public async Task Users_Create_Auth(string apiClientName)
+    public async Task Auth_Register_ReturnsOkResult_WithToken()
     {
         User? userInDb = null;
 
-        var userCreateJsonObject = new
-        {
-            Username = "User1",
-            Email = "User1@example.com",
-            FirstName = "User1First",
-            LastName = "User1Last",
-            Role = UserRoleEnum.User,
-            Password = "Password123",
-            ConfirmPassword = "Password123"
-        };
-        var jsonContent = new StringContent(JsonSerializer.Serialize(userCreateJsonObject), Encoding.UTF8, "application/json");
-
-        var url = "/users";
-
-        HttpResponseMessage? response;
-
         try
         {
-            switch (apiClientName)
+            var userCreateJsonObject = new
             {
-                case nameof(ApiClientWithNoAuth):
-                    response = await ApiClientWithNoAuth.PostAsync(url, jsonContent);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
-                    break;
-                case nameof(ApiClientWithRoleUser):
-                    response = await ApiClientWithRoleUser.PostAsync(url, jsonContent);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
-                    break;
-                case nameof(ApiClientWithRoleAdmin):
-                    response = await ApiClientWithRoleAdmin.PostAsync(url, jsonContent);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                    break;
-                case nameof(ApiClientWithRoleSuperAdmin):
-                    response = await ApiClientWithRoleSuperAdmin.PostAsync(url, jsonContent);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                    break;
-                default:
-                    throw new ArgumentException($"Invalid api client name: {apiClientName}");
-            }
+                Username = "User1",
+                Email = "User1@example.com",
+                FirstName = "User1First",
+                LastName = "User1Last",
+                Role = UserRoleEnum.User,
+                Password = "Password123",
+                ConfirmPassword = "Password123"
+            };
+
+            var jsonContent = new StringContent(JsonSerializer.Serialize(userCreateJsonObject), Encoding.UTF8, "application/json");
+
+            var response = await ApiClientWithRoleSuperAdmin.PostAsync("/register", jsonContent);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.IsSuccessStatusCode, Is.EqualTo(true));
+
+            var contentJsonString = await response.Content.ReadAsStringAsync();
+            var registerResponse = JsonSerializer.Deserialize<RegisterResponse>(contentJsonString, DefaultTestingJsonSerializerOptions);
+            Assert.That(registerResponse.Token, Is.Not.EqualTo(string.Empty));
+            Assert.That(registerResponse.Token, Is.Not.EqualTo(null));
+
+            userInDb = await DbUserGetByEmail(userCreateJsonObject.Email);
+
+            Assert.That(userInDb, Is.Not.EqualTo(null));
+            Assert.That(userInDb.Username, Is.EqualTo(userCreateJsonObject.Username));
+            Assert.That(userInDb.Email, Is.EqualTo(userCreateJsonObject.Email));
+            Assert.That(userInDb.FirstName, Is.EqualTo(userCreateJsonObject.FirstName));
+            Assert.That(userInDb.LastName, Is.EqualTo(userCreateJsonObject.LastName));
+            Assert.That(userInDb.Role, Is.EqualTo(userCreateJsonObject.Role));
+            Assert.That(userInDb.PasswordHash, Is.Not.EqualTo(null));
         }
         finally
         {
-            userInDb = await DbUserGetByEmail(userCreateJsonObject.Email);
             if (userInDb != null)
             {
                 await DbUserDeleteById(userInDb.Id);
             }
         }
     }
-
-    [Test]
-    [TestCaseSource(nameof(_apiClientNames))]
-    public async Task Users_Update_Auth(string apiClientName)
-    {
-        User? userInDb = null;
-
-        var newUser = new User
-        {
-            Username = "User1",
-            Email = "uusUser1@example.com",
-            FirstName = "User1First",
-            LastName = "User1Last",
-            Role = UserRoleEnum.User,
-            PasswordHash = "PasswordHash123",
-        };
-
-        var updateUserObject = new
-        {
-        };
-        JsonContent updateUserContent = JsonContent.Create(updateUserObject);
-
-        HttpResponseMessage? response;
-
-        try
-        {
-            await DbUserCreate(newUser);
-
-            userInDb = await DbUserGetByEmail(newUser.Email);
-            Assert.That(userInDb, Is.Not.EqualTo(null));
-
-            var url = $"/users/{userInDb.Id}";
-
-            switch (apiClientName)
-            {
-                case nameof(ApiClientWithNoAuth):
-                    response = await ApiClientWithNoAuth.PutAsync(url, updateUserContent);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
-                    break;
-                case nameof(ApiClientWithRoleUser):
-                    response = await ApiClientWithRoleUser.PutAsync(url, updateUserContent);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
-                    break;
-                case nameof(ApiClientWithRoleAdmin):
-                    response = await ApiClientWithRoleAdmin.PutAsync(url, updateUserContent);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                    break;
-                case nameof(ApiClientWithRoleSuperAdmin):
-                    response = await ApiClientWithRoleSuperAdmin.PutAsync(url, updateUserContent);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                    break;
-                default:
-                    throw new ArgumentException($"Invalid api client name: {apiClientName}");
-            }
-        }
-        finally
-        {
-            if (userInDb != null)
-            {
-                await DbUserDeleteByEmail(newUser.Email);
-            }
-        }
-    }
-
-    [Test]
-    [TestCaseSource(nameof(_apiClientNames))]
-    public async Task Users_Delete_Auth(string apiClientName)
-    {
-        User? userInDb = null;
-
-        var newUser = new User
-        {
-            Username = "User1",
-            Email = "uusUser1@example.com",
-            FirstName = "User1First",
-            LastName = "User1Last",
-            Role = UserRoleEnum.User,
-            PasswordHash = "PasswordHash123",
-        };
-
-        HttpResponseMessage? response;
-
-        try
-        {
-            await DbUserCreate(newUser);
-
-            userInDb = await DbUserGetByEmail(newUser.Email);
-            Assert.That(userInDb, Is.Not.EqualTo(null));
-
-            var url = $"/users/{userInDb.Id}";
-
-            switch (apiClientName)
-            {
-                case nameof(ApiClientWithNoAuth):
-                    response = await ApiClientWithNoAuth.DeleteAsync(url);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
-                    break;
-                case nameof(ApiClientWithRoleUser):
-                    response = await ApiClientWithRoleUser.DeleteAsync(url);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
-                    break;
-                case nameof(ApiClientWithRoleAdmin):
-                    response = await ApiClientWithRoleAdmin.DeleteAsync(url);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                    break;
-                case nameof(ApiClientWithRoleSuperAdmin):
-                    response = await ApiClientWithRoleSuperAdmin.DeleteAsync(url);
-                    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                    break;
-                default:
-                    throw new ArgumentException($"Invalid api client name: {apiClientName}");
-            }
-        }
-        finally
-        {
-            if (userInDb != null)
-            {
-                await DbUserDeleteByEmail(newUser.Email);
-            }
-        }
-    }
-
-    // Leaving here for now decide where to put them, need to test:
-    // Login
-    // Register (CreateUser)
-    // Tiered based role auth, i.e
-    //      If user is superadmin then should have access to everything below this
-    //      If user is admin should have admin and below, but not superadmin
 }
