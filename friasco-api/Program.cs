43eca801900d7d55f474d31ab9a1058126166ba6
“Taylor <t4ybennett@gmail.com>
2023-11-13 14:13:20 +0000

    Add bearer auth to swagger page

--- friasco-api/Program.cs
+++ friasco-api/Program.cs
@@ -9,6 +9,7 @@ using friasco_api.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Data.Sqlite;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 DotNetEnv.Env.Load();
 
@@ -85,6 +86,37 @@ builder.Services.AddAuthorization(options =>
     options.AddPolicy(nameof(UserRoleEnum.SuperAdmin), policy => policy.RequireRole(
         nameof(UserRoleEnum.SuperAdmin)
     ));
+});
+
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new() { Title = "FriascoApi", Version = "v1" });
+    x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.  \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
 });
 
 var app = builder.Build();
